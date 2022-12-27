d83953c2469b1d7e9b60188f0edfb07296300bb2
kenneth100417 <gisalankenneth3@gmail.com>
2022-12-27 06:53:37 -0800

    init

---
+++ js/menu-icon.js
@@ -0,0 +1,5 @@
+// menu-icon function
+function myFunction(x) {
+    x.classList.toggle("change");
+}
+// end 
---
+++ js/typing-animation.js
@@ -0,0 +1,8 @@
+
+var typed = new Typed(".auto-input",
+    {
+    strings: ["Kenneth Gisalan","Front-end Web Developer","Back-end Web Developer"],
+    typeSpeed: 90,
+    backSpeed: 40,
+    loop: true
+    })
